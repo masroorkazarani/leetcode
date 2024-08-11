a72ad6f6c8c475b8c00bdf53f581042015fb17de
masroorkazarani <119839620+masroorkazarani@users.noreply.github.com>
2024-08-11 18:03:59 +0530

    Time: 8 ms (33.85%) | Memory: 8.8 MB (69.60%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        
-        return isPalindromeRecursive(s, 0, s.length() - 1);
+        return checkString(s,0,s.length()-1);
     }
-    bool isPalindromeRecursive(const std::string& s, int left, int right) {
-        // Move left index forward if it points to a non-alphanumeric character
-        while (left < right && !std::isalnum(s[left])) {
+    bool checkString(const string &s,int left,int right){
+        while(left < right && !isalnum(s[left])){
             ++left;
         }
-        // Move right index backward if it points to a non-alphanumeric character
-        while (left < right && !std::isalnum(s[right])) {
+        while(left < right && !isalnum(s[right])){
             --right;
         }
-        // Base case: pointers have crossed or are pointing to the same character
-        if (left >= right) {
+        if(left>=right){
             return true;
         }
-        // Check if the current characters are equal
-        if (std::tolower(s[left]) != std::tolower(s[right])) {
+        if(tolower(s[left])!=tolower(s[right])){
             return false;
         }
-        // Recursively check the next pair of characters
-        return isPalindromeRecursive(s, left + 1, right - 1);
+
+        return checkString(s,left+1,right-1);
     }
 };
+
